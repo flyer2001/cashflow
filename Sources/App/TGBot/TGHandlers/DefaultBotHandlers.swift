de7452a21942a0d299c8d399b05bb3208af13510
Sergey Popyvanov <sgpopyyvanov@avito.ru>
2023-07-12 14:05:24 +0400

    попытки дебага через логгер

--- Sources/App/TGBot/TGHandlers/DefaultBotHandlers.swift
+++ Sources/App/TGBot/TGHandlers/DefaultBotHandlers.swift
@@ -83,8 +83,10 @@ final class HandlerFactory {
             
             await game.turn.startTurn()
             await game.dice.blockDice()
+            await App.bot.app.logger.debug("try send dice")
             let diceMessage = try await bot.sendDice(params: .init(chatId: .chat(chatId)))
             
+            await App.bot.app.logger.debug("wait dice")
             try await Task.sleep(nanoseconds: 3000000000)
             guard let diceResult = diceMessage.dice?.value else { return }
             
@@ -93,6 +95,7 @@ final class HandlerFactory {
                 [.init(text: "Завершить ход", callbackData: "endTurn")],
             ]
             
+            await App.bot.app.logger.debug("try send map")
             try await sendMap(
                 for: game.currentPlayerPosition,
                 chatId: chatId,
@@ -102,6 +105,7 @@ final class HandlerFactory {
                 completion: completion
             )
             await game.dice.resumeDice()
+            await App.bot.app.logger.debug("resume dice")
 //            try await Task.sleep(nanoseconds: 2000000000)
 //            try await App.deleteMessage(chatId: chatId, messageId: update.callbackQuery?.message?.messageId ?? 0)
 //            try await App.deleteMessage(chatId: chatId, messageId: diceMessage.messageId)
@@ -186,9 +190,12 @@ final class DefaultBotHandlers {
     }
     
     private static func playHandler() async {
+        await App.bot.app.logger.debug("create game")
         let game = Game()
+        await App.bot.app.logger.debug("create dice and endTurn handlerhandler")
         await buttonsActionHandler(game: game)
         
+        await App.bot.app.logger.debug("add start handler")
         await App.dispatcher.add(HandlerFactory.createPlayHandler(game: game))
     }
     
