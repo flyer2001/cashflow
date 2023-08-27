109fc6f745deac569545fa9cf0e0bce1d1750998
Sergey Popyvanov <sgpopyvanov@avito.ru>
2023-08-27 23:36:49 +0400

    11: Добавление логики передачи хода

--- Sources/App/TGBot/CashFlow/GameLogic.swift
+++ Sources/App/TGBot/CashFlow/GameLogic.swift
@@ -1,40 +1,64 @@
-//
-//  File.swift
-//  
-//
-//  Created by sgpopyvanov on 09.07.2023.
-//
-
 import Foundation
 
+enum GameError: Error {
+    case emptyPlayers
+}
+
 actor Game {
+    private(set) var players: [Player] = []
+    private(set) var adminId: Int64 = 0
+    
     let board: [String] = defaultBoard
-    var currentPlayerPosition: Int = 9
+    var currentPlayer: Player!
     let dice = Dice()
     let turn = Turn()
     
-    static func rollDice(numberOfDice: Int = 1) -> Int {
-        var result = 0
-        
-        for _ in 1...numberOfDice {
-            let diceRoll = Int.random(in: 1...6)
-            result += diceRoll
-        }
-        
-        return result
+    func addPlayer(_ id: Int64, name: String) {
+        guard !players.contains(where: { $0.id == id }) else { return }
+        let player = Player(id: id, name: name)
+        players.append(player)
+        currentPlayer = player
     }
     
-    func move(step: Int) -> String {
-        currentPlayerPosition += step
-        if currentPlayerPosition >= board.count {
-            currentPlayerPosition %= board.count
+    func shuffle() throws {
+        var shufflePlayers = players
+        
+        guard !players.isEmpty else {
+            throw GameError.emptyPlayers
         }
-        return board[currentPlayerPosition]
+        
+        for i in 0..<shufflePlayers.count {
+            let randomIndex = Int.random(in: 0..<shufflePlayers.count)
+            shufflePlayers.swapAt(i, randomIndex)
+        }
+        self.players = shufflePlayers
+        
+        guard let firstPlayer = players.first else {
+            throw GameError.emptyPlayers
+        }
+        self.currentPlayer = firstPlayer
     }
     
-    func reset() async {
-        currentPlayerPosition = 9
-        await turn.endTurn()
-        await dice.resumeDice()
+    func setAdmin(id: Int64) {
+        self.adminId = id
+    }
+    
+    func nextPlayer() {
+        guard let currentIndex = players.firstIndex(where: { $0.id == currentPlayer.id }) else {
+            return
+        }
+        if currentIndex == players.count - 1 {
+            currentPlayer = players[0]
+        } else {
+            currentPlayer = players[currentIndex + 1]
+        }
+    }
+    
+    func moveCurrentPlayer(step: Int) -> String {
+        currentPlayer.position += step
+        if currentPlayer.position >= board.count {
+            currentPlayer.position %= board.count
+        }
+        return board[currentPlayer.position]
     }
 }
---
+++ Tests/AppTests/GameLogicTests.swift
@@ -0,0 +1,27 @@
+@testable import App
+import XCTest
+
+final class GameLogicTests: XCTestCase {
+    var game: Game!
+
+    override func tearDownWithError() throws {
+        game = nil
+    }
+
+    func testNextPlayerMethod() async {
+        game = Game()
+        await game.addPlayer(1, name: "john")
+        await game.addPlayer(2, name: "scott")
+        await game.addPlayer(3, name: "mike")
+        
+        let currentId = await game.currentPlayer.id
+        
+        await game.nextPlayer()
+        await game.nextPlayer()
+        await game.nextPlayer()
+        
+        let lastId = await game.currentPlayer.id
+        
+        XCTAssertEqual(currentId, lastId)
+    }
+}
